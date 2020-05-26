0c89d64cee35528c79b56a3798fe542ced3b2a85
steel-a <stell4b@gmail.com>
2020-05-26 16:10:16 -0300

    Initial Commit

---
+++ python3/04-string.py
@@ -0,0 +1,32 @@
+n1 = "MariaDB"
+print('Len:',len(n1)) # 7
+print('Count:',n1.count('a')) # 2
+print('Count:',n1.count('a',3,10)) # 1
+print('Find:',n1.find("ria")) # 2
+print('Find:',n1.find("Paul")) # -1
+
+print('In:','ria' in n1) # True
+
+print(n1[2])       # r
+print(n1[2:5])     # ria
+print(n1[2:20])    # riaDB
+print(n1[1:20:2])  # aiD
+print(n1[:2])      # Ma
+print(n1[2:])      # riaDB
+print(n1[1::2])    # aiD
+
+print(n1.replace('ria','uau')) # MauauDB
+
+n2 = '     test    '
+print('Strip:',n2.strip())
+print('RStrip:',n2.rstrip())
+print('LStrip:',n2.lstrip())
+
+n3 = 'Lorem Ipsum is simply dummy text of the printing'
+splited = n3.split()
+print(splited[1]) # Ipsum
+print('-'.join(splited)) # Lorem-Ipsum-is-simply-dummy-text-of-the-printing
+
+n4 = """Lorem Ipsum is simply dummy text of the printing and
+typesetting industry. Lorem Ipsum has been the industry's
+standard dummy text ever since the 1500s"""
---
+++ python3/05-list.py
@@ -0,0 +1,12 @@
+from random import choice
+
+n1 = "Maria"
+n2 = "John"
+n3 = "Paul"
+n4 = "Luke"
+
+listNames = [n1, n2, n3, n4]
+print(choice(listNames))
+
+listNumbers = [1, 2, 3, 4, 5, 6]
+print(choice(listNumbers))
